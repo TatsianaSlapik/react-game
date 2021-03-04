82d8115ec1c8094b44df19f2057b84e7b8a21ce7
Tatsiana Slapik <slapik.tatiana@gmail.com>
2021-03-04 20:57:56 +0300

    Updated build

---
+++ precache-manifest.e2945ebe823f4df129319b58f50dc323.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbcbfac4a3096e08e9f0192b1b27985",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "da03153c762fce5cb81a",
+    "url": "/react-game/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "f87fb49f899bd9b4d50c",
+    "url": "/react-game/static/css/main.63630a9c.chunk.css"
+  },
+  {
+    "revision": "da03153c762fce5cb81a",
+    "url": "/react-game/static/js/2.5e99c655.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-game/static/js/2.5e99c655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73e8686e3bfa4f31e7d",
+    "url": "/react-game/static/js/3.ffcb5a07.chunk.js"
+  },
+  {
+    "revision": "f87fb49f899bd9b4d50c",
+    "url": "/react-game/static/js/main.820c3c1d.chunk.js"
+  },
+  {
+    "revision": "5a2549bdd48a9e5b7246",
+    "url": "/react-game/static/js/runtime-main.ce36ff2f.js"
+  },
+  {
+    "revision": "755c630f22bee6d73a9f902b1386fe06",
+    "url": "/react-game/static/media/final-redhat-and-grandma.755c630f.jpg"
+  },
+  {
+    "revision": "dbee9fcce1099ddd563f439a1ff63ea2",
+    "url": "/react-game/static/media/final-wolf.dbee9fcc.jpg"
+  },
+  {
+    "revision": "657e02067ec2e03b2e4d33c2a0594a7f",
+    "url": "/react-game/static/media/gameMusic.657e0206.mp3"
+  },
+  {
+    "revision": "e4f8156faf46cc19ff689da3142e433c",
+    "url": "/react-game/static/media/grandma.e4f8156f.png"
+  },
+  {
+    "revision": "ceef0927301d1b40a3981caf31fb5ce3",
+    "url": "/react-game/static/media/house.ceef0927.png"
+  },
+  {
+    "revision": "79395290c69ed8ed5ea3601a169397a8",
+    "url": "/react-game/static/media/redHat.79395290.png"
+  },
+  {
+    "revision": "fa592fb564c34da3ed5eb78cd4e72d87",
+    "url": "/react-game/static/media/stepSound.fa592fb5.mp3"
+  },
+  {
+    "revision": "e48294035441f707efbddac01fa6f4a8",
+    "url": "/react-game/static/media/tree.e4829403.png"
+  },
+  {
+    "revision": "19241363248aa79bc4be7ac721e1976d",
+    "url": "/react-game/static/media/wolf.19241363.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63b4ecd0469ba690e42853bbe6863f17.js"
+  "/react-game/precache-manifest.e2945ebe823f4df129319b58f50dc323.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
